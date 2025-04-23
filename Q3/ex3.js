027938deec3c077f14444620cdfad351b9c9fe3b
Alessandro Mendes <165946341+AlessandroMendesS@users.noreply.github.com>
2025-04-23 14:46:49 -0300

    Add files via upload

---
+++ Q3/ex3.js
@@ -0,0 +1,11 @@
+const clube = document.getElementById('input').value;
+const AdicionarTime = document.getElementById('adicionarTime');
+const MostrarTime = document.getElementById('botaoMostrar');
+const ListarTimes = document.getElementById('botaoListar');
+const times = document.getElementById('times');
+const rivais = document.getElementById('vs');
+
+function AdicionarClubes () {
+    
+}
+
